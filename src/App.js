d0c76dfe3db77d1167bf253896504ccf35de87af
LazarevEV <lazarev.evgeniy.nf@gmail.com>
2022-05-20 22:39:03 +0300

    Added: Fiels Copied

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react'
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import About from './page/About'
+import BookCart from './components/BookCart'
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div className="1">
+			<div className="App">
+				<Routes>
+					<Route path="/" element={<BookCart />} />
+					<Route path="about" element={<About />} />
+				</Routes>
+			</div>
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
