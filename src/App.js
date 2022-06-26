969aeb291ef196918ecf99cd4e47ddb2130981c0
LazarevEV <lazarev.evgeniy.nf@gmail.com>
2022-06-26 19:02:36 +0300

    HW13

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import { Routes, Route } from 'react-router-dom'
 import About from './page/About'
 import NotFound from './page/NotFound'
 import OrderTotals from './page/OrderTotals'
-import SelectTask from './page/SelectTask'
 import BookCart from './components/BookCart'
 import LoginPass from './components/LoginPass'
-import ArrayMapTask from './page/ArrayMapTask'
-import LoginPassHW10 from './components/LoginPassHW10'
-import ShowHideTextBlock from './components/ShowHideTextBlock'
-import SquaresTask from './components/SquaresTask'
 
 function App() {
 	return (
@@ -21,11 +16,6 @@ function App() {
 					<Route path="/cart" element={<BookCart/>} />
 					<Route path="/cart_totals" element={<OrderTotals/>} />
 					<Route path="/about" element={<About />} />
-					<Route path="/selectTask" element={<SelectTask />} />
-					<Route path="/arrayMapTask" element={<ArrayMapTask />} />
-					<Route path="/loginPassHW10" element={<LoginPassHW10 />} />
-					<Route path="/spoilerHW11" element={<ShowHideTextBlock />} />
-					<Route path="/squaresHW11" element={<SquaresTask />} />
 					<Route path="*" element={<NotFound />} />
 				</Routes>
 			</div>
--- src/components/LoginPass.js
+++ src/components/LoginPass.js
@@ -1,51 +1,72 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import useInputRequired from '../hooks/useInputRequired'
 import { Link, useNavigate } from 'react-router-dom'
 
 const IS_LOGIN_REQUIRED = true;
 const IS_PASSWORD_REQUIRED = true;
 
-export default function LoginPass() {
+const ERROR_MESSAGES = {
+    emptyField: "This field is required!",
+    loginInvalid: "Login is invalid! Expected smth like example@example.com",
+    passInvalid: "Password is invalid! Length should be at least 6 characters."
+}
+
+export default function LoginPassHW10() {
     const [creds, inputChangeHandler, onBlurHandler, inputState, setInputState] = useInputRequired({});
     const navigate = useNavigate();
 
+    const loginInputRef = useRef(null);
+    const passInputRef = useRef(null);
+    const loginInputError = useRef(null);
+    const passInputError = useRef(null);
+
     const validateCreds = () => {
         let count = 0;
-        [...document.querySelectorAll('input')].forEach((inputElement) => {
-            if (inputElement.className === 'login' && IS_LOGIN_REQUIRED) {
-                // count += (inputElement.value === "") ? 1 : 0;
-                // setInputState({...inputState, [inputElement.className]: (inputElement.value === "") ? 0 : 1});
-                count += (validateLogin()) ? 0 : 1;
-                setInputState({...inputState, [inputElement.className]: (validateLogin()) ? 1 : 0});
-            }
+        count += (IS_PASSWORD_REQUIRED) ? setError("password", passInputRef, passInputError) : 0
+        count += (IS_LOGIN_REQUIRED) ? setError("login", loginInputRef, loginInputError) : 0
 
-            if (inputElement.className === 'password' && IS_PASSWORD_REQUIRED) {
-                // count += (inputElement.value === "") ? 1 : 0;
-                // setInputState({...inputState, [inputElement.className]: (inputElement.value === "") ? 0 : 1});
-                count += (validatePassword()) ? 0 : 1;
-                setInputState({...inputState, [inputElement.className]: (validatePassword()) ? 1 : 0});
-            }
-        })
-
+        // if (count === 0) navigate('/cart', {replace: true})
         if (count === 0) navigate('/cart', {replace: true})
     }
 
     const validateLogin = () => {
-        return (/\S+@\S+\.\S+/.test((creds.login || ""))) ? true : false;
+        if (!creds.login) return "emptyField"
+        return (/\S+@\S+\.\S+/.test((creds.login || ""))) ? "loginValid" : "loginInvalid";
     }
 
     const validatePassword = () => {
-        return ((creds.password  || '').length >= 6) ? true : false;
+        if (!creds.password) return "emptyField"
+        return ((creds.password  || '').length >= 6) ? "passValid" : "passInvalid";
+    }
+
+    const setError = (className, inputRef, errorRef) => {
+        setInputState(inputState => ({...inputState, [inputRef.current.name]: (validationResultBool) ? 1 : 0}));
+        // alert(inputState.login + " " + inputState.password)
+
+        const validationResult = (className === "login") ? validateLogin() : validatePassword()
+        const validationResultBool = (className === "login") ? validationResult === "loginValid" : validationResult === "passValid"
+
+        if (!validationResultBool) {
+            // alert(0)
+            inputRef.current.classList.add("error")
+            inputRef.current.focus()
+            errorRef.current.textContent = ERROR_MESSAGES[validationResult]
+        } else {
+            // alert(1)
+            inputRef.current.classList.remove("error")
+        }
+
+        return (validationResultBool) ? 0 : 1;
     }
 
     return (
         <div className="cred-wrapper">
             <label htmlFor="login-input">Login:</label>
-            <input id="login-input" name="login" className="login" onChange={inputChangeHandler} onBlur={(event) => onBlurHandler(event, IS_LOGIN_REQUIRED)}></input>
-            <span className="error-message login">This field is required!</span>
+            <input ref={loginInputRef} id="login-input" name="login" className="login" onChange={inputChangeHandler} onBlur={(event) => onBlurHandler(event, IS_LOGIN_REQUIRED, validateLogin() === "loginValid")}></input>
+            <span ref={loginInputError} className="error-message login">This field is required!</span>
             <label htmlFor="pass-input">Password:</label>
-            <input id="pass-input" name="password" className="password" onChange={inputChangeHandler} onBlur={(event) => onBlurHandler(event, IS_PASSWORD_REQUIRED)}></input>
-            <span className="error-message password">This field is required!</span>
+            <input ref={passInputRef} id="pass-input" name="password" className="password" onChange={inputChangeHandler} onBlur={(event) => onBlurHandler(event, IS_PASSWORD_REQUIRED, validatePassword() === "passValid")}></input>
+            <span ref={passInputError} className="error-message password">This field is required!</span>
             <button type="submit" className="button" onClick={validateCreds}>LOG IN</button>
         </div>
     )
